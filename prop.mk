ae4eaab90f012facca8a4b38c76b305f6c66f0c5
Dyneteve <dyneteve@pixelexperience.org>
2020-07-09 11:31:16 +0530

    vince: enable aosp surfaceflinger
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -179,12 +179,13 @@ vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.set_display_power_timer_ms=10000 \
+ro.surface_flinger.set_idle_timer_ms=9000
+ro.surface_flinger.set_touch_timer_ms=5000 \
+ro.surface_flinger.use_smart_90_for_video=true \
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
