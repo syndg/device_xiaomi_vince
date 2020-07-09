948728b665e43898d29a85a4e649e33322972134
kerneltoast <kerneltoast@users.noreply.github.com>
2020-07-09 12:36:46 +0530

    vince: force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -229,6 +229,7 @@ vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.set_display_power_timer_ms=10000 \
