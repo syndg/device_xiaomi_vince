1a84ac06f28a80a18fc4e77b71b71ae95cd0eed6
sinchandasgupta <sinchandsgpt9@gmail.com>
2020-07-31 06:18:23 +0000

    vince: Fluid-ify
    Signed-off-by: sinchandasgupta <sinchandsgpt9@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vince.mk
+    $(LOCAL_DIR)/fluid_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_vince-user \
-    aosp_vince-userdebug \
-    aosp_vince-eng
+    fluid_vince-user \
+	fluid_vince-userdebug \
+    fluid_vince-eng
---
+++ fluid-config.mk
@@ -0,0 +1,15 @@
+# Some common FLuid Flags
+
+# Gapps flags
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+# Whether or not to disable the default camera
+DISABLE_DEFAULT_CAMERA := true
+
+# Whether or not target supports google recoder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Build Type
+CUSTOM_BUILD_TYPE := OFFICIAL
