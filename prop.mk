0bb5f52573269d1ecde396780686d03683a4d817
iamsaalim <saalimquadri1@gmail.com>
2020-08-15 00:02:20 +0530

    vince: Nuke some perf stuff
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -177,8 +177,7 @@ vendor.video.disable.ubwc=1
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.fw.dex2oat_thread_count=8 \
-ro.vendor.extension_library=libqti-perfd-client.so
+ro.sys.fw.dex2oat_thread_count=8
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
