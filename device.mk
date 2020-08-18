7f9a82eee99e0e7925df5dbbd7a8c9be83720b5a
starlight5234 <ifist2834@gmail.com>
2020-08-19 02:57:38 +0530

    vince: Initial Powerhint
    * remove powerhint.xml
    
    Signed-off-by: starlight5234 <ifist2834@gmail.com>

--- device.mk
+++ device.mk
@@ -307,7 +307,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.vince-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # QMI
