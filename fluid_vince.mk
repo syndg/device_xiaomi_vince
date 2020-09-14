0a7514ef1e89ae714b4fb623360e930cf774aba4
KenHV <kenhv@null.net>
2020-09-14 21:57:17 +0530

    vince: update coral fp to september patch
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: sinchandasgupta <sinchandsgpt9@gmail.com>

--- fluid_vince.mk
+++ fluid_vince.mk
@@ -40,4 +40,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
