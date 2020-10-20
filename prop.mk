8e6b0717cbba2162b9fce6d5ea7d90176b0d4379
sinchandasgupta <sinchandsgpt9@gmail.com>
2020-10-20 10:03:32 +0000

    vince: Enable native blur
    Signed-off-by: sinchandasgupta <sinchandsgpt9@gmail.com>

--- prop.mk
+++ prop.mk
@@ -215,6 +215,11 @@ ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
+# Blur
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.supports_background_blur=1 \
+ro.sf.blurs_are_expensive=1
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.delta_time.enable=true
