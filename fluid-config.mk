e6e94434e3770306e5973826e6d7a7fd03dbe39a
sinchandasgupta <sinchandsgpt9@gmail.com>
2020-12-26 12:00:24 +0530

    fluid-config: Make Some Changes
    Signed-off-by: sinchandasgupta <sinchandsgpt9@gmail.com>

--- fluid-config.mk
+++ fluid-config.mk
@@ -1,13 +1,9 @@
 # Some common FLuid Flags
-
 # Gapps flags
 TARGET_INCLUDE_GAPPS := true
-$(call inherit-product, vendor/gms/gms_full.mk)
-#TARGET_GAPPS_ARCH := arm64
-#IS_PHONE := true
-
-# Whether or not to disable the default camera
-DISABLE_DEFAULT_CAMERA := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_INCLUDE_MINIMAL_GAPPS := true
 
 # Whether or not target supports google recoder
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
